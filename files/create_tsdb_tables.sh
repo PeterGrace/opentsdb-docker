4c8af8cc461523fc9d0ae0e1145796f406676d40
Mincheol Kim <mcauto@naver.com>
2020-11-27 15:11:21 +0900

    Update create_tsdb_tables.sh

--- files/create_tsdb_tables.sh
+++ files/create_tsdb_tables.sh
@@ -5,5 +5,8 @@ export HBASE_HOME=/opt/hbase
 export TSDB_VERSION="::TSDB_VERSION::"
 export JAVA_HOME="::JAVA_HOME::"
 
+# https://github.com/OpenTSDB/opentsdb/issues/1481
+sed -i "s/, TTL => '\$TSDB_TTL'//g" /usr/local/share/opentsdb/tools/create_table.sh
+
 /usr/local/share/opentsdb/tools/create_table.sh
 touch /data/hbase/opentsdb_tables_created.txt
